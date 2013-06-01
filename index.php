16194bb67e7abb4cd38211a0a40bd8bbdff35000
Thomas Moreau <tomamoral@gmail.com>
2013-06-01 14:50:33 -0700

    premier pas HTML

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Maxi blog du Van VW :)</title>
+  </head>
+
+  <body>
+    <div id="bloc_page">
+      <header>
+        <div id="titre_principal">
+          <img src="images/camper.png" alt="Logo VW" id="logo" />
+          <h1>VW Bus</h1>
+          <h2>Carnets de voyage</h2>
+        </div>
+         
+        <nav>
+          <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Blog</a></li>
+         </ul>
+        </nav>
+      </header>
+         
+      <div id="banniere_image">
+        <div id="banniere_description">
+          Blog du VW bus...
+          <a href="#" class="bouton_rouge">
+            Voir l'article 
+            <img src="images/flecheblanchedroite.png" alt="" />
+          </a>
+        </div>
+      </div>
+       
+      <section>
+        <article>
+          <h1><img src="images/ico_epingle.png" alt="Catégorie voyage"
+          class="ico_categorie" />Post</h1>
+          <p>On est au top au soleil de Californie...</p>
+          <p>On va faire un maxi road trip en van VW...</p>
+          <p>Et on fait de la planche et des hackerton...</p>
+        </article>
+
+        <aside>
+          <h1>Funny fact</h1>
+          <img src="images/bulle.png" alt="" id="fleche_bulle" />
+          <p id="photo_encart"><img src="images/zozor_classe.png" alt="Photo de fun" /></p>
+          <p>
+            C'est la class, on geek un max n'est ce pas?
+          </p>
+          <p> 
+            Ce blog sera alimente par mes fautes d'orthographe et mon nouveau
+            Nexus4.... yeah yeah :)
+          </p>
+          </aside>
+      </section>
+       
+      <footer>
+        <div id="fact">
+          <h1>Dernier Pouletty fact</h1>
+          <p>Il a de nouveau dormis dehors!</p>
+          <p>le 12 mai à 23h12</p>
+        </div>
+        <div id="mes_photos">
+          <h1>Mes photos</h1>
+          <p><img src="images/photo1.jpg" alt="Photographie" /><img src="images/photo2.jpg" alt="Photographie" /><img src="images/photo3.jpg" alt="Photographie" /><img src="images/photo4.jpg" alt="Photographie" /></p>
+        </div>
+        <div id="mes_amis">
+          <h1>Mes amis</h1>
+          <ul>
+            <li><a href="#">Pupi le lapin</a></li>
+            <li><a href="#">Mr Baobab</a></li>
+            <li><a href="#">Kaiwaii</a></li>
+            <li><a href="#">Perceval.eu</a></li>
+          </ul>
+          <ul>
+            <li><a href="#">Belette</a></li>
+            <li><a href="#">Le concombre masqué</a></li>
+            <li><a href="#">Ptit prince</a></li>
+            <li><a href="#">Mr Fan</a></li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>

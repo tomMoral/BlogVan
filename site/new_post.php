8ea69a2cd87da8b5b60e405ab414c5988ecf4e32
gsabran <sabranguillaume@gmail.com>
2013-06-19 09:55:13 -0700

    image handeling changed

--- site/new_post.php
+++ site/new_post.php
@@ -1,5 +1,4 @@
 <?php
-
 include("headerPHP.php");
 $user = user::getSessionUser();
 if ($user != null && $user->type == 2) {
@@ -11,10 +10,11 @@ if ($user != null && $user->type == 2) {
                 <td>
                     <div id="post">
                         <form action="new_post.php" method="post" enctype="multipart/form-data" id="np">
+                            <input type="hidden" name="visualization" id="visualization" value=0/>
                             Titre: <input type="text" name="title" id="title"></br>
-                            Post: <textarea type="text" id="postarea" name="post" placeholder='New Post, insert photo at [pi]'></textarea><br>
+                            Post: <textarea type="text" id="postarea" name="post"  class="new_post" placeholder='New Post, insert photo at [pi]'></textarea><br>
                             ([prop1:prop2:...:propn] for a vote)<br>
-                            (photo [p1]...[p9])<br>
+                            (photo p1...p9)<br>
                             Permission: <input type="checkbox" name="permission" value=1 checked="checked">All<br>
                             <input type="submit"></br>
                             pics :</br><input type="file" name="pic1" id="pic1"></br>
@@ -33,8 +33,8 @@ if ($user != null && $user->type == 2) {
                                     less than one minute ago
                                 </legend>
                             </div>
-                            <p>
-                            <div id="bodyvisualization"></div>
+                            <p id="bodyvisualization">
+                            <div></div>
                             </p>
                         </article>
 
@@ -44,23 +44,8 @@ if ($user != null && $user->type == 2) {
         </table>
 
         <script>
-            function on_change($input, $i) {
-                return (function() {
-                    if ($input.value == '')
-                        return;
-                    var $newFile = document.createElement('input');
-                    $newFile.type = "file";
-                    $newFile.name = "pic" + $i;
-                    $newFile.onchange = on_change($newFile, $i + 1);
-                    $input.parentNode.appendChild($newFile);
-                    $input.parentNode.appendChild(document.createElement('br'));
-
-                })
-            }
-            $inp = document.getElementById('pic1')
-            $inp.onchange = on_change($inp, 2);
-
             $(document).ready(function() {
+                $('textarea').autosize();
                 $("#postarea").keyup(function() {
                     var text = document.getElementById("postarea").value;
                     $("#bodyvisualization").html(text);
@@ -69,43 +54,147 @@ if ($user != null && $user->type == 2) {
                     var text = document.getElementById("title").value;
                     $("#titlevisualization").html(text);
                 });
+                $("#pic1").change(function() {
+                    $("#visualization").attr("value", 1);
+                    $("#np").submit();
+                });
+                $("#np").submit(function() {
+                });
             });
+
         </script>
         <?php
-
     } else {
-        $i = 1;
-        $pics = '';
-        $dossier = 'pics_up/';
-        echo 'hello';
-        print_r($_FILES);
-        while (isset($_FILES["pic$i"])) {
-            echo $i;
-            echo $_FILES["pic$i"]['name'];
-            $fichier = basename($_FILES["pic$i"]['name']);
-            if (move_uploaded_file($_FILES["pic$i"]['tmp_name'], $dossier . $fichier)) { //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-                echo 'Upload effectué avec succès !';
-                $pics .=',' . Photos::add_photo('', $dossier . $fichier);
-            } else { //Sinon (la fonction renvoie FALSE).
-                echo 'Echec de l\'upload !';
+        if ($_POST['visualization'] == 0) {
+            $i = 1;
+            $pics = '';
+            $dossier = 'pics_up/';
+            while (isset($_FILES["pic$i"])) {
+                $fichier = basename($_FILES["pic$i"]['name']);
+                if (move_uploaded_file($_FILES["pic$i"]['tmp_name'], $dossier . $fichier)) { //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+                    $pics .=',' . Photos::add_photo('', $dossier . $fichier);
+                } else { //Sinon (la fonction renvoie FALSE).
+                }
+                $i += 1;
             }
-            $i += 1;
-        }
-        $perm = (isset($_POST['permission'])) ? 1 : 0;
-        $count = preg_match_all('/\[([^:]+:)+[^\]]+\]/', $_POST['post']);
-        if ($count == 0) {
-            $comments = '';
+            $perm = (isset($_POST['permission'])) ? 1 : 0;
+            $count = preg_match_all('/\[([^:]+:)+[^\]]+\]/', $_POST['post']);
+            if ($count == 0) {
+                $comments = '';
+            } else {
+                $comments = 'v';
+            }
+            echo $pics;
+            $return = Posts::add_post('GPS1', $_POST['title'], $_POST['post'], $pics, $comments, $perm);
+            if ($return == '') {
+                header("Location: index.php");
+            } else {
+                echo $return;
+            }
+            exit;
         } else {
-            $comments = 'v';
+            //upload the image
+            $i = 1;
+            $dossier = 'pics_up/';
+            while (isset($_FILES["pic$i"])) {
+                $fichier = basename($_FILES["pic$i"]['name']);
+                if (move_uploaded_file($_FILES["pic$i"]['tmp_name'], $dossier . $fichier)) { //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+                } else { //Sinon (la fonction renvoie FALSE).
+                }
+                $i += 1;
+            }
+            htmlHeader("blog");
+            ?>
+            <table>
+                <tr>
+                    <td>
+                        <div id="post">
+                            <form action="new_post.php" method="post" enctype="multipart/form-data" id="np">
+                                <input type="hidden" name="visualization" id="visualization" value=0/>
+                                Titre: <input type="text" name="title" id="title" value="<?php echo $_POST['title']; ?>"></br>
+                                Post: <textarea type="text" id="postarea" class="new_post" name="post" placeholder='New Post, insert photo at [pi]'><?php echo $_POST['post']; ?></textarea><br>
+                                ([prop1:prop2:...:propn] for a vote)<br>
+                                (photo [p1]...[p9])<br>
+                                Permission: <input type="checkbox" name="permission" value=<?php echo $perm = (isset($_POST['permission'])) ? 1 : 0; ?> checked="checked">All<br>
+                                <input type="submit"></br>
+                                pics :</br><input type="file" name="pic1" id="pic1"></br>
+                            </form>
+                        </div>
+                    </td>
+                    <td>
+                        <section class="post">
+                            <article>
+                                <div class="title">
+                                    <h1>
+                                        <div id="titlevisualization"></div>
+
+                                    </h1>
+                                    <legend>
+                                        less than one minute ago
+                                    </legend>
+                                </div>
+                                <p id="bodyvisualization">
+                                <div></div>
+                                </p>
+                            </article>
+
+                        </section>
+                    </td>
+                </tr>
+            </table>
+
+            <script>
+
+
+                $(document).ready(function() {
+                   // $('textarea').autosize();
+                    var pic = [<?php
+            $i = 1;
+            while (isset($_POST["p$i"])) {
+                echo $i == 1 ? "" : ",";
+                echo "{code: \"p$i\", name:\"" . $_POST["p$i"] . "\"}";
+                $i++;
+            }
+            echo $i == 1 ? "" : ",";
+            echo "{code: \"p$i\", name:\"" . $dossier . $fichier . "\"}";
+            ?>];
+                    for (var i = 0; i < pic.length; i++) {
+                        $("form").append("</br>" + pic[i].code + " : " + pic[i].name + "  upload succeeded!");
+                    }
+                    var text = document.getElementById("postarea").value;
+                    for (var i = 0; i < pic.length; i++) {
+                        var re = new RegExp(pic[i].code, 'g');
+                        text = text.replace(re, '<img src="' + pic[i].name + '" style="max-width: 530px"/>');
+                    }
+                    $("#bodyvisualization").html(text);
+                    $("textarea").html(text);
+                    var text = document.getElementById("title").value;
+                    $("#titlevisualization").html(text);
+
+                    $("#postarea").keyup(function() {
+                        var text = document.getElementById("postarea").value;
+                        for (var i = 0; i < pic.length; i++) {
+                            var re = new RegExp(pic[i].code, 'g');
+                            text = text.replace(re, '<img src="' + pic[i].name + '" style="max-width: 530px"/>');
+                        }
+                        $("#bodyvisualization").html(text);
+                        $("textarea").html(text);
+                    });
+                    $("#title").keyup(function() {
+                        var text = document.getElementById("title").value;
+                        $("#titlevisualization").html(text);
+                    });
+                    $("#pic1").change(function() {
+                        $("#visualization").attr("value", 1);
+                        for (var i = 0; i < pic.length; i++) {
+                            $("form").append("<input type=\"hidden\" name=\"" + pic[i].code + "\" value =\"" + pic[i].name + "\"/>");
+                        }
+                        $("#np").submit();
+                    });
+                });
+
+            </script><?php
         }
-        echo $pics;
-        $return = Posts::add_post('GPS1', $_POST['title'], $_POST['post'], $pics, $comments, $perm);
-        if ($return == '') {
-            header("Location: index.php");
-        } else {
-            echo $return;
-        }
-        exit;
     }
 } else {
     header('Location: index.php');

23137cb9d22c8f0b52eb74928b4346f4e91f84d6
gsabran <sabranguillaume@gmail.com>
2013-06-20 11:49:03 -0700

    @ added, better display, refresh possible

--- site/new_post.php
+++ site/new_post.php
@@ -5,19 +5,18 @@ if ($user != null && $user->type == 2) {
     if (!isset($_POST['post'])) {
         htmlHeader("blog");
         ?>
-        <table>
+        <table class="tablepost">
             <tr>
-                <td>
-                    <div id="post">
+                <td >
+                    <div id="post" style="position:absolute; top:0px">
                         <form action="new_post.php" method="post" enctype="multipart/form-data" id="np">
                             <input type="hidden" name="visualization" id="visualization" value=0/>
-                            Titre: <input type="text" name="title" id="title"></br>
-                            Post: <textarea type="text" id="postarea" name="post"  class="new_post" placeholder='New Post, insert photo at pi'></textarea><br>
-                            ([prop1:prop2:...:propn] for a vote)<br>
-                            (photo @0...@9)<br>
-                            Permission: <input type="checkbox" name="permission" value=1 checked="checked">All<br>
-                            <input type="submit"></br>
-                            pics :</br><input type="file" name="pic1" id="pic1"></br>
+                            <input type="text" name="title" id="title" placeholder="Title"></br></br>
+                            <textarea type="text" id="postarea" name="post"  class="new_post" placeholder='New Post, insert photo at @i, vote at [prop1:prop2:...:propn]'></textarea></br><br>
+                            
+                            Permission: <input type="checkbox" name="permission" value=1 checked="checked">All</br></br>
+                            <input type="submit"></br></br>
+                            pics :</br><div id="image"><input type="file" name="pic1" id="pic1"></div></br>
                         </form>
                     </div>
                 </td>
@@ -108,20 +107,18 @@ if ($user != null && $user->type == 2) {
             }
             htmlHeader("blog");
             ?>
-            <table>
-                <tr>
-                    <td>
-                        <div id="post">
+        <table class="tablepost">
+            <tr>
+                <td >
+                    <div id="post" style="position:absolute; top:0px">
                             <form action="new_post.php" method="post" enctype="multipart/form-data" id="np">
                                 <input type="hidden" name="visualization" id="visualization" value=0/>
-                                Titre: <input type="text" name="title" id="title" value="<?php echo $_POST['title']; ?>"></br>
-                                Post: <textarea type="text" id="postarea" class="new_post" name="post" placeholder='New Post, insert photo at pi'><?php echo $_POST['post']; ?></textarea><br>
-                                ([prop1:prop2:...:propn] for a vote)<br>
-                                (photo [p1]...[p9])<br>
-                                Permission: <input type="checkbox" name="permission" value=<?php echo $perm = (isset($_POST['permission'])) ? 1 : 0; ?> checked="checked">All<br>
-                                <input type="submit"></br>
-                                pics :</br><input type="file" name="pic1" id="pic1"></br>
-                            </form>
+                                <input type="text" name="title" id="title" value="<?php echo $_POST['title']; ?>" placeholder="Title"></br></br>
+                                <textarea type="text" id="postarea" class="new_post" name="post" placeholder='New Post, insert photo at @i, vote at [prop1:prop2:...:propn]'><?php echo $_POST['post']; ?></textarea></br><br>
+                                Permission: <input type="checkbox" name="permission" value=<?php echo $perm = (isset($_POST['permission'])) ? 1 : 0; ?> checked="checked">All  <img src="../images/refresh.png" id="refresh" style="width:25px; position: absolute; left:230px;"/><br></br>
+                                <input type="submit"></br></br>
+                                pics :</br><input type="file" name="pic1" id="pic1"></br></br>
+                            </form><input type="submit" name="refresh" id="refresh" value="refresh"></br>
                         </div>
                     </td>
                     <td>
@@ -152,18 +149,18 @@ if ($user != null && $user->type == 2) {
                 $(document).ready(function() {
                     var pic = [<?php
             $i = 1;
-            while (isset($_POST["p$i"])) {
+            while (isset($_POST["@$i"])) {
                 echo $i == 1 ? "" : ",";
-                echo "{code: \"p$i\", name:\"" . $_POST["p$i"] . "\"}";
+                echo "{code: \"@$i\", name:\"" . $_POST["@$i"] . "\"}";
                 $i++;
             }
-            if ($upload_succeed) {
+            if (isset($upload_succeed) && $upload_succeed) {
                 echo $i == 1 ? "" : ",";
-                echo "{code: \"p$i\", name:\"" . $dossier . $fichier . "\"}";
+                echo "{code: \"@$i\", name:\"" . $dossier . $fichier . "\"}";
             }
             ?>];
                     for (var i = 0; i < pic.length; i++) {
-                        $("form").append("</br>" + pic[i].code + " : " + pic[i].name + "  upload succeeded!");
+                        $("form").append("</br>" + pic[i].code + " : " + pic[i].name + " <br/> upload succeeded!");
                     }
                     var text = document.getElementById("postarea").value;
                     for (var i = 0; i < pic.length; i++) {
@@ -175,6 +172,7 @@ if ($user != null && $user->type == 2) {
                     var text = document.getElementById("title").value;
                     $("#titlevisualization").html(text);
 
+                    $('textarea').autosize();
                     $("#postarea").keyup(function() {
                         var text = document.getElementById("postarea").value;
                         for (var i = 0; i < pic.length; i++) {
@@ -187,12 +185,21 @@ if ($user != null && $user->type == 2) {
                     $("#title").keyup(function() {
                         var text = document.getElementById("title").value;
                         $("#titlevisualization").html(text);
+                        document.getElementById('postarea').value=text;
                     });
                     $("#pic1").change(function() {
                         $("#visualization").attr("value", 1);
                         for (var i = 0; i < pic.length; i++) {
                             $("form").append("<input type=\"hidden\" name=\"" + pic[i].code + "\" value =\"" + pic[i].name + "\"/>");
                         }
+                        $("#np").submit();
+                    });
+                    $("#refresh").click(function(){
+                     $("#visualization").attr("value", 1);
+                        for (var i = 0; i < pic.length; i++) {
+                            $("form").append("<input type=\"hidden\" name=\"" + pic[i].code + "\" value =\"" + pic[i].name + "\"/>");
+                        }
+                        $("#pic1").remove();
                         $("#np").submit();
                     });
                 });

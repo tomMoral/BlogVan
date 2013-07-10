994451eab91dd97b7c874c8a65dd5f7de9049e48
gsabran <sabranguillaume@gmail.com>
2013-07-10 11:51:46 -0700

    music

---
+++ site/ajax/load_music.php
@@ -0,0 +1,66 @@
+<?php
+$A = scandir("../../sounds/music");
+for ($i = 0; $i < count($A); $i++) {
+    if (substr($A[$i], 0, 1) != ".") {
+        $A[$i] = "../sounds/music/" . $A[$i];
+    } else {
+        $A[$i] = "";
+    }
+}
+shuffle($A);
+?>
+<script>
+
+    $(document).ready(function() {
+        var songs = [<?php
+$count = 0;
+for ($i = 0; $i < count($A); $i++) {
+    echo $A[$i] == "" ? "" : "\"".$A[$i] . "\",";
+    $count+= $A[$i] == "" ? 0 : 1;
+}
+?>];
+        var nb_songs =<?php echo $count; ?>;
+        var song_num = 0;
+        var audio = new Audio();
+        function load() {
+            audio.src = songs[song_num % nb_songs];
+            song_num++;
+            audio.load();
+        }
+        function myPlay() {
+            audio.play();
+            audio.addEventListener('ended', function() {
+                load();
+                myPlay();
+            });
+        }
+        audio.src = songs[song_num % nb_songs];
+        song_num++;
+        audio.load();
+        audio.play();
+        audio.pause();
+        audio.addEventListener('ended', function() {
+            load();
+            myPlay();
+        });
+
+
+
+
+        $("#music").hide();
+        var playing = 0;
+        $("#manage_music").click(function() {
+            playing = playing === 0 ? 1 : 0;
+            if (playing === 1) {
+                audio.play();
+                $("#manage_music img").attr("src", "../images/pause.png");
+            }
+            else {
+                audio.pause();
+                $("#manage_music img").attr("src", "../images/play.png");
+            }
+        });
+
+
+    });
+</script>
--- site/new_post.php
+++ site/new_post.php
@@ -8,6 +8,7 @@ if ($user != null && $user->type == 2) {
         <table class="tablepost">
             <tr>
                 <td >
+<<<<<<< Updated upstream
 
                     <div id="post" style="position:absolute; top:0px; width:270px;">
                         <form action="new_post.php" method="post" enctype="multipart/form-data" id="np">
@@ -21,6 +22,15 @@ if ($user != null && $user->type == 2) {
                             Permission: <input type="checkbox" name="permission" value=1 checked="checked">All<img src="../images/refresh.png" id="refresh" style="width:25px; position: absolute; left:230px;"/></br>
                             </br>Attention, set the permission before choosing the photos</br></br>
 
+=======
+                    <div id="post" style="position:absolute; top:0px">
+                        <form action="new_post.php" method="post" enctype="multipart/form-data" id="np">
+                            <input type="hidden" name="visualization" id="visualization" value=0/>
+                            <input type="text" name="title" id="title" placeholder="Title"></br></br>
+                            <textarea type="text" id="postarea" name="post"  class="new_post" placeholder='New Post, insert photo at @i, vote at [prop1:prop2:...:propn]'></textarea></br><br>
+                            
+                            Permission: <input type="checkbox" name="permission" value=1 checked="checked">All</br></br>
+>>>>>>> Stashed changes
                             <input type="submit"></br></br>
                             pics :</br><div id="image"><input type="file" name="pic1" id="pic1"></div></br>
                         </form>
@@ -150,6 +160,7 @@ if ($user != null && $user->type == 2) {
             }
             htmlHeader("blog");
             ?>
+<<<<<<< Updated upstream
 
             <table class="tablepost">
                 <tr>
@@ -166,6 +177,17 @@ if ($user != null && $user->type == 2) {
                                 </br>Attention, set the permission before choosing the photos
                                 </br></br>
 
+=======
+        <table class="tablepost">
+            <tr>
+                <td >
+                    <div id="post" style="position:absolute; top:0px">
+                            <form action="new_post.php" method="post" enctype="multipart/form-data" id="np">
+                                <input type="hidden" name="visualization" id="visualization" value=0/>
+                                <input type="text" name="title" id="title" value="<?php echo $_POST['title']; ?>" placeholder="Title"></br></br>
+                                <textarea type="text" id="postarea" class="new_post" name="post" placeholder='New Post, insert photo at @i, vote at [prop1:prop2:...:propn]'><?php echo $_POST['post']; ?></textarea></br><br>
+                                Permission: <input type="checkbox" name="permission" value=<?php echo $perm = (isset($_POST['permission'])) ? 1 : 0; ?> checked="checked">All  <img src="../images/refresh.png" id="refresh" style="width:25px; position: absolute; left:230px;"/><br></br>
+>>>>>>> Stashed changes
                                 <input type="submit"></br></br>
                                 pics :</br><input type="file" name="pic1" id="pic1"></br></br>
                             </form><input type="submit" name="refresh" id="refresh" value="refresh"></br>
@@ -228,8 +250,6 @@ if ($user != null && $user->type == 2) {
                                 echo "{code: \"@$i\", name:\"" . $dossier . $fichier . "\"}";
                             }
                             ?>];
-
-
                     for (var i = 0; i < pic.length; i++) {
                         $("form").append("</br>" + pic[i].code + " : " + pic[i].name + " <br/> upload succeeded!");
                     }
@@ -255,7 +275,6 @@ if ($user != null && $user->type == 2) {
                     var text = document.getElementById("titleFrench").value;
                     $("#titlevisualizationFrench").html(text);
 
-
                     $('textarea').autosize();
                     $("#postarea").keyup(function() {
                         lastFieldUsed = 1;
@@ -279,7 +298,11 @@ if ($user != null && $user->type == 2) {
                     $("#title").keyup(function() {
                         var text = document.getElementById("title").value;
                         $("#titlevisualization").html(text);
+
                         document.getElementById('postarea').value = text;
+
+                        document.getElementById('postarea').value=text;
+
                     });
 
                     $("#postareaFrench").keyup(function() {
@@ -324,18 +347,19 @@ if ($user != null && $user->type == 2) {
                         $("#np").submit();
                     });
 
+
             <?php echo $_POST['lastUsed'] == 1 ? '$("#postarea").focus();' : '$("#postareaFrench").focus();'; ?>
 
 
                     $("#refresh").click(function() {
                         $("#visualization").attr("value", 1);
+
                         for (var i = 0; i < pic.length; i++) {
                             $("form").append("<input type=\"hidden\" name=\"" + pic[i].code + "\" value =\"" + pic[i].name + "\"/>");
                         }
                         $("#pic1").remove();
                         $("#np").submit();
                     });
-
                 });
 
             </script><?php

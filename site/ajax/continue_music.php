23d207bc6ee44809be3781ef2dabb64e35c331fc
gsabran <sabranguillaume@gmail.com>
2013-07-11 21:22:05 -0700

    music qhould be working (on firefox at least)

---
+++ site/ajax/continue_music.php
@@ -0,0 +1,15 @@
+<?php
+include_once("../headerPHP.php"); 
+if(isset($_POST['songs'])){
+    $_SESSION['songs']=$_POST['songs'];
+}
+if(isset($_POST['playing'])){
+    $_SESSION['playing']=$_POST['playing'];
+}
+if(isset($_POST['currentTime'])){
+    $_SESSION['currentTime']=$_POST['currentTime'];
+}
+if(isset($_POST['song_num'])){
+    $_SESSION['song_num']=$_POST['song_num'];
+}
+?>
--- site/quicktest.php
+++ site/quicktest.php
@@ -1,37 +1,66 @@
-<?php
-if (isset($_FILES["file"])) {
-    print_r($_FILES['file']);
-    $allowedExts = array("gif", "jpeg", "jpg", "png");
-    if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "image/x-png") || ($_FILES["file"]["type"] == "image/png"))) {
-        if ($_FILES["file"]["error"] > 0) {
-            echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-        } else {
-            echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-            echo "Type: " . $_FILES["file"]["type"] . "<br>";
-            echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-            echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-
-            if (file_exists("upload/" . $_FILES["file"]["name"])) {
-                echo $_FILES["file"]["name"] . " already exists. ";
-            } else {
-                move_uploaded_file($_FILES["file"]["tmp_name"], "" . $_FILES["file"]["name"]);
-                echo "Stored in: " . "" . $_FILES["file"]["name"];
-            }
-        }
-    } else {
-        echo "Invalid file";
-    }
-}
-?>
+<!DOCTYPE html>
 <html>
-    <body>
+    <head>
+        <meta charset='utf-8' />
+       
+    </head>
+    <body > <style>
+            h1, h2, h3, h4, h5, h6 {
+                font-family: 'sofiapro',Arial,sans-serif;
+                font-weight: normal;
+                text-rendering: optimizelegibility;
+                margin: 0px 0px;
+            }strong {
+                font-weight: bold;
+            }ul, ol {
+                margin: 0 0 15px 25px;
+                padding: 0;
+            }
+        </style>
+        <div id='test' style='margin:40px; width: 600px; text-align: justify; color: #333333;
+          font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+          font-size: 13px;
+          line-height: 21px;'><br/>
+            <h1 >UX Designer
+            </h1><br/><h2>Guillaume Sabran</h2><br/>
+            <br/>           Dear Sir or Madam,
+            <br/>You will find in this document my motivations to work at Coursera as an UX designer and the reasons why I think I will be an excellent fit for this position. Thanks a lot by advance for your attention!</br>
+            <br/><strong>My motivations:</strong>
+            <ul><li>I'm a smart, creative and passionate person willing to change the face of education and I'm very interested in working with smart, creative and passionate people pursuing the same goal. Bringing free and accessible high quality education to everyone is a huge challenge.</li>
+                <li>Having worked as a Math teacher for six months in Benin I'm well aware of the major educative issues you are tackling. I think my passion and my desire to change things come from this complex and beautiful experience.</li>
+                <li>I love the experience I've had at Coursera so far and I want to make it even better for everyone.</li></ul>
 
-        <form action="quicktest.php" method="post"
-              enctype="multipart/form-data">
-            <label for="file">Filename:</label>
-            <input type="file" name="file" id="file"><br>
-            <input type="submit" name="submit" value="Submit">
-        </form>
-
+            <strong>My qualities:</strong>
+            <ul><li>Strong capacity for empathy which enable me to understand quickly someone else's needs and reactions.</li>
+                <li>Natural gift for Mathematics and one of the best formation in this field received. Many think that Maths require rigor and complexity; I would say they are about creativity and understanding. I'm indeed a very creative person.</li>
+                <li> My engineer formation and my very good knowledge of web languages give me an excellent understanding of the tools you have to build you revolutionary product.</li>
+            </ul>
+            Since product design is about understanding a need and coming with the best feasible solution to address it, I'm convince that, having those three qualities, I will be a very good fit for the position you are offering.</br>
+            </br><strong>My experiences:</strong>
+            <ul><li>Every time I had the opportunity to do so, I took design classes such as Product Design or Interaction Design – those classes appears on my transcript under the beautiful codes HSS2 & HSS3... They have been an excellent complement to the engineer formation I received at Ecole Polytechnique, France leading university.</li>
+            <li> Not being a morning person, I created in one of those classes an alarm clock which was like a balloon going up while playing music. To snooze it, one had to make bigger and bigger movement as it goes higher, waking him up slowly but surely.</li></ul>
+             <br/><br/><strong>My ideas to improve user commitment at Coursera:</strong>
+             <ul><li>Start the deadline calendar when one enrolls. I imagine the issues it would create but waiting for a class to begin or always been behind of schedule are not engaging experiences.</li>
+                 <li>In most African countries, Internet access is developing especially on phones and students have access to education. What makes a difference is their ability to pay for private teacher. Besides, education is highly valued and students are used to work together after school as it is free and the bigger resource they have is time, not money. Thus, if you could create classes on how to succeed in the particular exercises they are asked to do and which they value, replacing the costly private teacher, I'm convinced it would have a great success. Plus, the peer to peer correction and discussion forum you have are very close to the mutual aid there are used to.</li>
+                 <li>I would love to thank the designer who decided to add a + button below the videos. This single button made me love Coursera.</li></ul>
+            <br/>
+            <strong>Other:</strong>
+            <ul><li>While I was studying at Ecole Polytechnique, I was a member of the student board. As such, I worked closely with its administration. So, if at some point you want to extend your partnership with this university, I may be able to help if needed.</li>
+                <li>I am looking for an internship starting in September. I'm interning at UCBerkeley and I would be delighted to come to your office to discuss the possibility of working with you. I'm passionated and willing to work hard to lower educative barriers.</li>
+                <li>You can find what I'm working on at github.com/gsabran</li>
+                <li>I'm creating a blog at california-here-we.com If you enjoy the design or have any suggestions to improve it, please tell me!</li>
+                <li>You will find attached to my resume my last transcripts.</li>
+                <li>The French-American Chamber of Commerce confirmed that I was eligible for a J-1 visa.</li></ul>
+     
+           	I'm taking classes at Coursera. The experience you are providing is incredible and it is exciting to see the improvements which regularly appear, showing that you are continuously improving your platform. I would love to be part of you active team and help millions of students have access to a better education.
+            <br/>
+            <br/>I'm really looking forward to hearing from you.
+            <br/>
+            <br/>Sincerely,
+            <br/>
+            <br/>Guillaume Sabran
+            <br/>	
+            <br/>PS: I have a very good knowledge of HTML5, JavaScript, CSS3, PHP, jQuery, Ajax and MySQL as I love building and designing cool stuff. If you think I would be a better fit for another position, especially as a product software engineer, please let me know.
+        </div>
     </body>
-</html>  
+</html>

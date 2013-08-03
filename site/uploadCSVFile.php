f434ccfb5f79f3354ca724f1d3fdd1e40b06971a
Thomas Moreau <tomamoral@gmail.com>
2013-08-02 19:10:03 -0700

    Fix position usage

--- site/uploadCSVFile.php
+++ site/uploadCSVFile.php
@@ -7,7 +7,7 @@ if (isset($user) && $user->type == 2) {
         foreach ($file as $line_number => $line) {
             $tab = explode(",", $line);
             print_r($tab);
-            $time = 0;//date("Y-m-d H:i:s",$tab[0]);
+            $time = date("Y-m-d H:i:s",$tab[0]/1000);
             $lat = $tab[1];
             $lon = $tab[2];
             $accuracy = $tab[3];
@@ -15,7 +15,6 @@ if (isset($user) && $user->type == 2) {
             echo $line . "<br/>";
         }
     }
-    echo date();
     ?>
     <form action="uploadCSVFile.php" method="post" enctype="multipart/form-data" id="np">    
         file :<input type="file" name="file"/>
